ef0a94b26ed33d8662eb968da8dfa29650d2cdff
Austin Cooper <cooperaustinj@gmail.com>
2020-02-03 19:18:25 -0600

    updates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const request = require('request');
 require('log-timestamp');
 
-const url = `http://mcapi.us/server/status?ip=${process.env.SERVER_IP}`;
+const url = `http://mcapi.us/server/query?ip=${process.env.SERVER_IP}`;
 
 const client = new Discord.Client();
 client.login(process.env.DISCORD_TOKEN);
@@ -31,4 +31,38 @@ client.on('ready', () => {
     console.log('Client connected');
     updateStatus();
     setInterval(updateStatus, 2 * 60 * 1000);
+});
+
+client.on('message', msg => {
+    if (msg.author.equals(client.user)) {
+        return;
+    }
+    if (msg.channel.type !== 'dm') {
+        return;
+    }
+    switch (msg.content) {
+        case 'who':
+            request(url, function (err, res, body) {
+                if (err) {
+                    console.log(err);
+                    msg.reply('Error getting server info');
+                    return;
+                }
+                body = JSON.parse(body);
+                if (!body.online) {
+                    msg.reply('Server is offline');
+                    return;
+                }
+                if (!body.players.now) {
+                    msg.reply('No players online');
+                    return;
+                }
+                msg.reply(body.players.list);
+            });
+            break;
+
+        default:
+            msg.reply('Unknown command');
+            break;
+    }
 });
